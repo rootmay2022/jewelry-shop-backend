5eedcdbb6fc35d61bd99138b49e404bd00bc2397
nguyen khanh hung <nguyenkhanhhung1531@gmail.com>
2026-01-26 23:41:41 +0700

    fix: thêm quen pass

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -19,7 +19,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/auth")
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*")
 public class AuthController {
